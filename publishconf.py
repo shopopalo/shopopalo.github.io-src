f2f5d13b2dce1a8ebe3d5ce043e3aee721199651
Toshyk <phonxis@gmail.com>
2016-07-03 15:09:11 +0300

    change http to httpS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://shopopalo.github.io'
+SITEURL = 'https://shopopalo.github.io'	# 'http://shopopalo.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
